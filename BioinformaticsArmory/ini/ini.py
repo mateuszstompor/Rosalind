e05a02b319254d1851ac514150197dc0d206bcde
Mateusz Stompór <mateusz.stompor@gmail.com>
2019-01-19 22:55:25 +0100

    Add solution to "Introduction to the Bioinformatics Armory"

---
+++ BioinformaticsArmory/ini/ini.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def nucleotide_occurrences(sequence):
+    occurrences = {'A': 0, 'C': 0, 'T': 0, 'G': 0}
+    for letter in sequence:
+        occurrences[letter] += 1
+    return occurrences
+
+
+if __name__ == "__main__":
+    print("There is possibility to pass a path to file containing data as a first argument")
+    if len(sys.argv) > 2:
+        print('Wrong Invocation! Pass a path to file or do not pass anything')
+    else:
+        filename = sys.argv[1] if len(sys.argv) == 2 else 'rosalind_ini.txt'
+        with open(filename) as file:
+            sequence = file.read().strip()
+            occurrences = nucleotide_occurrences(sequence)
+            print(occurrences['A'], occurrences['C'], occurrences['G'], occurrences['T'])
