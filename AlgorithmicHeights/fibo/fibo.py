4872487f17ba35621456f3e3b438142cc8d82457
Mateusz Stompór <mateusz.stompor@gmail.com>
2019-01-19 18:04:04 +0100

    Add solution to "Fibonacci Numbers"

---
+++ AlgorithmicHeights/fibo/fibo.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Invoke the program passing a path to a file as an argument")
+    else:
+        with open(sys.argv[1]) as file:
+            number = int(file.read())
+            print(fibonacci(number))
